5dbe595e5e839468d741796db3d87abd1a233008
Nikolas <callmenik2018@outlook.com>
2025-10-31 09:32:23 -0700

    Added LocalDate instance data and constructor to Score class.

--- src/code/Score.java
+++ src/code/Score.java
@@ -3,4 +3,20 @@ import java.time.format.DateTimeFormatter;
 
 public class Score
 {
+    private final LocalDate currentTime;
+    private final DateTimeFormatter formatter;
+    private final String formattedDateTime;
+
+    public Score()
+    {
+        this.currentTime = LocalDate.now();
+        this.formatter = DateTimeFormatter.ofPattern("yyyyy-MM-dd HH:mm:ss");
+        this.formattedDateTime = this.currentTime.format(formatter);
+    }
+
+    void displayFormattedDateTime()
+    {
+        System.out.println(this.formattedDateTime);
+    }
+
 }
